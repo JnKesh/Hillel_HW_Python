950a2e6f6829f1dd21b782b4fdec8dc359d1410e
Ispravnikov V <vit.ispra@gmail.com>
2021-01-29 23:55:48 +0200

    update1

--- HW2/Escape sequences.py
+++ HW2/Escape sequences.py
@@ -1,2 +1,1 @@
-
 print("""Escape sequences\n\\a\t\tBell (alert)\n\\b\t\tBackspace\n\\n\t\tNew line\n\\t\t\tHorizontal tab\n\\\ \t\tBackslash \\\n\\"\t\tDouble quotation mark \"\n\\'\t\tSingle quotation mark \'""")
