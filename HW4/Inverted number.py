bfb14a0401e59cd327d88147a6d68e0f80e4d5bf
Ispravnikov V <vit.ispra@gmail.com>
2021-01-31 21:23:44 +0200

    HW4/Inverted number.py UPDATE

--- HW4/Inverted number.py
+++ HW4/Inverted number.py
@@ -2,4 +2,4 @@ a = int(input("Enter 3-digit number: "))
 b = a % 10  # First number
 c = (a % 100) // 10  # Second number
 d = (a % 1000) // 100  # Third number
-print("Inverted number is:", b, c, d)
+print("Inverted number is:", b * 100 + c * 10 + d)
