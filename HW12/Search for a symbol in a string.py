2a278bf5d97b703f9734eaae45eeb72b24efe2ea
Ispravnikov V <vit.ispra@gmail.com>
2021-02-20 02:59:07 +0200

    Update0220

--- HW12/Search for a symbol in a string.py
+++ HW12/Search for a symbol in a string.py
@@ -1,14 +1,8 @@
 s = str(input('Enter text here: '))
 ch = str(input('Enter character to find here: '))
-start = 0
-
-while ch in s:
-    start = s.find(ch, start+1)  # Поиска символа
-    for i in range(len(s)):  # Его индекс
-        if s[i] == ch:
-            index = i
-            print('Matches found at the index:', index)
-    break
-else:
-    print('No matches found')
-print('Searching complete.')
+start = -1
+while True:
+    start = s.find(ch, start + 1)
+    if start == -1:
+        break
+    print('Matches found at the index:', start)
--- HW16/Removing a list item.py
+++ HW16/Removing a list item.py
@@ -3,7 +3,7 @@ from random import randint  # Отсылка к серверу за рандом
 a = [randint(1, 20) for _ in range(10)]  # Обращение к серверу за рандомными числами
 print(", ".join(str(e) for e in a))  # Вывод списка с рандомными числами
 k = int(input('Enter index: '))  # Индекс элемента в списке
-for i in range(k + 1, len(a)):  # Сдвиг списка
-    a[i - 1] = a[i]
+for i in range(k + 1, len(a)):
+    a[i - 1] = a[i]  # Сдвиг списка
 a.pop()
 print(', '.join([str(i) for i in a]))
