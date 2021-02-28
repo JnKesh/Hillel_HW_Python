b2c77b8dfa37c9eee48cd913895dfe4a62fba28e
Ispravnikov V <vit.ispra@gmail.com>
2021-02-28 12:51:04 +0200

    update0228

--- HW18/The ledger.py
+++ HW18/The ledger.py
@@ -1,4 +1,7 @@
-arr_orders = [[34587, 'Learning Python, Mark Lutz', 4, 40.95],
-             [98762, 'Programming Python, Mark Lutz', 5, 56.80],
-             [77226, 'Head First Python, Paul Barry', 3, 32.95],
-             [88112, 'Einfuhrung in Python3, Bernd Klein', 3, 24.99]]
+a = [[34587, 'Learning Python, Mark Lutz', 4, 40.95],
+     [98762, 'Programming Python, Mark Lutz', 5, 56.80],
+     [77226, 'Head First Python, Paul Barry', 3, 32.95],
+     [88112, 'Einfuhrung in Python3, Bernd Klein', 3, 24.99]]
+
+b = list(map(lambda x: (x[0], round(x[2] * x[3] if x[2] * x[3] > 100 else x[2] * (x[3] + 10), 2)), a))
+print(b)
